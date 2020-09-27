35ac383db1a9306e77afad2889b45b493fca8d36
Nikita <nicitazaletin@yandex.ru>
2020-09-27 13:49:52 +0300

    refactor:createUI

--- app/src/main/java/com/example/visit/CreateFragment.java
+++ app/src/main/java/com/example/visit/CreateFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +16,13 @@ public class CreateFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_create,container,false);
+        Button saveBtn = view.findViewById(R.id.save_button);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
         return view;
     }
 }
