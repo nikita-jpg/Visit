b36ef89e38ff81ead32ae1fa759a87877b9d16be
Nikita <nicitazaletin@yandex.ru>
2020-10-02 14:57:45 +0300

    Fix:input layout

--- app/src/main/java/com/example/visit/createteamivent/CreateTeamEvent.java
+++ app/src/main/java/com/example/visit/createteamivent/CreateTeamEvent.java
@@ -4,6 +4,7 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
